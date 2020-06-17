cc55676f0f21503ab946fdbee866f23d055153da
Dmitry Gusarov <1462777+gusarov@users.noreply.github.com>
2020-06-17 14:34:09 -0400

    first commit

---
+++ SimpleGrep/Executor.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SimpleGrep
+{
+	class Executor
+	{
+		public string Dir { get; set; }
+		public List<string> Files { get; } = new List<string>();
+
+		public List<string> Patterns { get; } = new List<string>();
+		public bool Recursive { get; set; }
+
+		private readonly List<Regex> _regexes = new List<Regex>();
+
+		public void Execute()
+		{
+			foreach (var pattern in Patterns)
+			{
+				_regexes.Add(new Regex(pattern, RegexOptions.Compiled));
+			}
+
+			if (string.IsNullOrWhiteSpace(Dir))
+			{
+				throw new Exception("Directory not defined");
+			}
+
+			if (!Directory.Exists(Dir))
+			{
+				throw new Exception("Directory does not exists");
+			}
+
+
+			foreach (var filePattern in Files)
+			{
+				if (filePattern.Contains('*') || filePattern.Contains('?'))
+				{
+					foreach (var file in Directory.GetFiles(Dir, filePattern, Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly))
+					{
+						var subPath = file.Substring(Dir.Length + 1);
+						Process(subPath);
+					}
+				}
+				else
+				{
+					Process(file: filePattern);
+				}
+			}
+		}
+
+		void PrintFileName(string file)
+		{
+			Console.WriteLine();
+
+			var fileName = Path.GetFileName(file);
+			var fileNameWithoutExt = Path.GetFileNameWithoutExtension(file);
+			var ext = fileName.Substring(fileNameWithoutExt.Length);
+
+			var path = file.Substring(0, file.Length - fileName.Length);
+
+			using (Color(ConsoleColor.DarkCyan))
+			{
+				Console.Write(path);
+			}
+			using (Color(ConsoleColor.Cyan))
+			{
+				Console.Write(fileNameWithoutExt);
+			}
+			using (Color(ConsoleColor.DarkCyan))
+			{
+				Console.WriteLine(ext);
+			}
+		}
+
+		IDisposable Color(ConsoleColor color)
+		{
+			return new ColorScope(color);
+		}
+
+		class ColorScope : IDisposable
+		{
+			private readonly ConsoleColor _original;
+
+			public ColorScope(ConsoleColor color)
+			{
+				_original = Console.ForegroundColor;
+				Console.ForegroundColor = color;
+			}
+
+			public void Dispose()
+			{
+				Console.ForegroundColor = _original;
+			}
+		}
+
+		void Process(string file)
+		{
+			bool filePrinted = false;
+			var lines = File.ReadAllLines(file);
+			foreach (var line in lines)
+			{
+				foreach (var regex in _regexes)
+				{
+					int printPosition = 0;
+					var matches = regex.Matches(line);
+					if (matches.Any())
+					{
+						if (!filePrinted)
+						{
+							filePrinted = true;
+							PrintFileName(file);
+						}
+					}
+					foreach (Match match in matches)
+					{
+						if (match.Index > printPosition)
+						{
+							using (Color(ConsoleColor.Gray))
+							{
+								Console.Write(line.Substring(printPosition, match.Index - printPosition));
+							}
+						}
+						using (Color(ConsoleColor.Red))
+						{
+							Console.Write(match.Value);
+						}
+						printPosition = match.Index + match.Length;
+					}
+
+					if (matches.Any())
+					{
+						using (Color(ConsoleColor.Gray))
+						{
+							Console.WriteLine(line.Substring(printPosition));
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ SimpleGrep/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SimpleGrep
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			var executor = new Executor();
+
+			var listRemaining = new LinkedList<string>();
+
+			for (var i = 0; i < args.Length; i++)
+			{
+				var arg = args[i];
+				if (arg.StartsWith("-") || arg.StartsWith("/"))
+				{
+					switch (arg.Substring(1))
+					{
+						case "r":
+						case "R":
+							executor.Recursive = true;
+							break;
+						case "e":
+							executor.Patterns.Add(args[i++]);
+							break;
+						case "f":
+							executor.Files.Add(args[i++]);
+							break;
+						case "v":
+							// invert
+							break;
+					}
+				}
+				else
+				{
+					listRemaining.AddLast(arg);
+				}
+			}
+
+			// capture implicit pattern
+			if (listRemaining.Any() && !executor.Patterns.EmptyIfNull().Any())
+			{
+				executor.Patterns.Add(listRemaining.First.Value);
+				listRemaining.RemoveFirst();
+			}
+
+			// capture files
+			foreach (var arg in listRemaining)
+			{
+				executor.Files.Add(arg);
+			}
+
+			if (executor.Patterns.Count == 0)
+			{
+				Console.WriteLine("Pattern is not specified");
+				return 1;
+			}
+
+			executor.Dir = Directory.GetCurrentDirectory();
+			executor.Execute();
+			return 0;
+		}
+	}
+}
